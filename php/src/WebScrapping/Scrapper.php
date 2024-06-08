00d6f74d5b9ebb9b794b1e5e78f6ed20dff7355d
Camila <camila0wolter@gmail.com>
2024-06-08 12:22:39 -0300

    feat: get HTML document elements with respective classes

--- php/src/WebScrapping/Scrapper.php
+++ php/src/WebScrapping/Scrapper.php
@@ -14,17 +14,16 @@ class Scrapper {
    * Loads paper information from the HTML and returns the array with the data.
    */
   public function scrap(\DOMDocument $dom): array {
-    return [
-      new Paper(
-        123,
-        'The Nobel Prize in Physiology or Medicine 2023',
-        'Nobel Prize',
-        [
-          new Person('Katalin Karikó', 'Szeged University'),
-          new Person('Drew Weissman', 'University of Pennsylvania'),
-        ]
-      ),
-    ];
+    $xpath = new DOMXPath($dom);
+
+    $papers = [];
+    $authors = [];
+
+    $titleElements = $xPATH->query('//*[@class="my-xs paper-title"]');
+    $authorsElements = $xPATH->query('//*[@class="authors"]');
+    $idElements = $xPATH->query(('//*[@class="volume-info"]'));
+    $postTypeElements = $xPATH->query(('//*[@class="tags mr-sm"]'));
+
   }
 
 }
