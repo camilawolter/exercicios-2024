a915b6bc1e76524216573823f08d10c8c6e1d819
Camila <camila0wolter@gmail.com>
2024-06-08 18:27:02 -0300

    chore: fixing formatting

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -8,13 +8,13 @@ namespace Chuva\Php\WebScrapping;
 class Main {
 
 	/**
-	 * Main runner, instantiates a Scrapper and runs.
+	* Main runner, instantiates a Scrapper and runs.
   */
   public static function run(): void {
-		$html = file_get_contents(__DIR__ . '/../../assets/origin.html');
+    $html = file_get_contents(__DIR__ . '/../../assets/origin.html');
     $dom = new \DOMDocument('1.0', 'utf-8');
     $dom->loadHTML($html);
-
+    
     $data = (new Scrapper())->scrap($dom);
     $spreadsheet = new Spreadsheet();
 
