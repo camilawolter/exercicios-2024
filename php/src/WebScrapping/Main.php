8caa628b1dd7339e0f2c69e1f7ffcd88299c1151
Camila <camila0wolter@gmail.com>
2024-06-08 16:41:19 -0300

    feat: changing the html reading and calling the spreadsheet creation function

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -11,14 +11,15 @@ class Main {
    * Main runner, instantiates a Scrapper and runs.
    */
   public static function run(): void {
-    $html = file_get_contents(__DIR__ . '/../../assets/origin.html');
+    $html = file_get_contents(__DIR__.'/../../assets/origin.html');
     $dom = new \DOMDocument('1.0', 'utf-8');
-    $dom->loadHTMLFile($html);
+    $dom->loadHTML($html);
 
     $data = (new Scrapper())->scrap($dom);
+    $spreadsheet = new Spreadsheet();
 
     // Write your logic to save the output file bellow.
-    print_r($data);
+     $spreadsheet->createSpreadsheet($data);
   }
 
 }
