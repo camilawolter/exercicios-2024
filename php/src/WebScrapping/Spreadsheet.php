37f53719bec348fa9197fc0a791df40b2542f852
Camila <camila0wolter@gmail.com>
2024-06-08 17:32:03 -0300

    feat: adding style to the header

--- php/src/WebScrapping/Spreadsheet.php
+++ php/src/WebScrapping/Spreadsheet.php
@@ -2,65 +2,73 @@
 
 namespace Chuva\Php\WebScrapping;
 
-use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
 use Box\Spout\Writer\Common\Creator\Style\StyleBuilder;
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
 use Box\Spout\Common\Entity\Style\CellAlignment;
 use Exception;
 
 class Spreadsheet
 {
-    public function createSpreadsheet(array $papers): void
-    {
-        try {
-            $filePath = __DIR__ . '/../../assets/result.xlsx';
-            $writer = WriterEntityFactory::createXLSXWriter();
-            $writer->openToFile($filePath);
+  public function createSpreadsheet(array $papers): void
+  {
+    try {
+      $filePath = __DIR__ . '/../../assets/result.xlsx';
+      $writer = WriterEntityFactory::createXLSXWriter();
+      $writer->openToFile($filePath);
 
-            // cabeçalho da tabela
-            $headerCells = [
-                'ID', 'Title', 'Type',
-                'Author 1', 'Author 1 Institution',
-                'Author 2', 'Author 2 Institution',
-                'Author 3', 'Author 3 Institution',
-                'Author 4', 'Author 4 Institution',
-                'Author 5', 'Author 5 Institution',
-                'Author 6', 'Author 6 Institution',
-                'Author 7', 'Author 7 Institution',
-                'Author 8', 'Author 8 Institution',
-                'Author 9', 'Author 9 Institution'
-            ];
+      // Estilo do cabeçalho da tabela.
+      $headerStyle = (new StyleBuilder())
+        ->setFontBold()
+        ->setFontName('Arial')
+        ->setFontSize(10)
+        ->setCellAlignment(CellAlignment::LEFT)
+        ->build();
 
-            $headerRow = WriterEntityFactory::createRowFromArray($headerCells);
-            $writer->addRow($headerRow);
+      // Cabeçalho da tabela.
+      $headerCells = [
+        'ID', 'Title', 'Type',
+        'Author 1', 'Author 1 Institution',
+        'Author 2', 'Author 2 Institution',
+        'Author 3', 'Author 3 Institution',
+        'Author 4', 'Author 4 Institution',
+        'Author 5', 'Author 5 Institution',
+        'Author 6', 'Author 6 Institution',
+        'Author 7', 'Author 7 Institution',
+        'Author 8', 'Author 8 Institution',
+        'Author 9', 'Author 9 Institution'
+      ];
 
-            // estilo padrão para as células
-            $defaultStyle = (new StyleBuilder())
-                ->setFontName('Arial')
-                ->setFontSize(10)
-                ->setCellAlignment(CellAlignment::LEFT)
-                ->setShouldWrapText()
-                ->build();
+      $headerRow = WriterEntityFactory::createRowFromArray($headerCells, $headerStyle);
+      $writer->addRow($headerRow);
 
-            // preenchendo a planilha com os dados
-            foreach ($papers as $paper) {
-                $valueCells = [];
-                $valueCells[] = $paper->id;
-                $valueCells[] = $paper->title;
-                $valueCells[] = $paper->type;
+      // Estilo padrão para as células.
+      $defaultStyle = (new StyleBuilder())
+        ->setFontName('Arial')
+        ->setFontSize(10)
+        ->setCellAlignment(CellAlignment::LEFT)
+        ->setShouldWrapText()
+        ->build();
 
-                foreach ($paper->authors as $author) {
-                    $valueCells[] = str_replace(';', '', $author->name);
-                    $valueCells[] = $author->institution;
-                }
+      // Preenchendo a planilha com os dados.
+      foreach ($papers as $paper) {
+        $valueCells = [];
+        $valueCells[] = $paper->id;
+        $valueCells[] = $paper->title;
+        $valueCells[] = $paper->type;
 
-                $valueRow = WriterEntityFactory::createRowFromArray($valueCells, $defaultStyle);
-                $writer->addRow($valueRow);
-            }
+      foreach ($paper->authors as $author) {
+        $valueCells[] = str_replace(';', '', $author->name);
+        $valueCells[] = $author->institution;
+      }
 
-            $writer->close();
-            print_r("Planilha criada com sucesso");
-        } catch (Exception $e) {
-            print_r("Ocorreu um erro: " . $e->getMessage());
-        }
+      $valueRow = WriterEntityFactory::createRowFromArray($valueCells, $defaultStyle);
+      $writer->addRow($valueRow);
     }
+
+      $writer->close();
+      print_r("Planilha criada com sucesso");
+    } catch (Exception $e) {
+      print_r("Ocorreu um erro: " . $e->getMessage());
+    }
+  }
 }
