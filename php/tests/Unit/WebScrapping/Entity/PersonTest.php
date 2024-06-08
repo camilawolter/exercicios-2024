3d79f3647ea81af64faccf6f4fad3c81b5dfee3d
Camila <camila0wolter@gmail.com>
2024-06-08 17:09:55 -0300

    test: changing postId test

--- php/tests/Unit/WebScrapping/Entity/PersonTest.php
+++ php/tests/Unit/WebScrapping/Entity/PersonTest.php
@@ -14,22 +14,22 @@ class PersonTest extends TestCase {
    * Tests construct().
    */
   public function testConstruct() {
-    $person = new Person('Name', 'Institution', '111');
+    $person = new Person('Name', 'Institution', '1234');
 
     $this->assertEquals('Name', $person->name);
     $this->assertEquals('Institution', $person->institution);
-    $this->assertEquals('111', $person->postId);
+    $this->assertEquals('1234', $person->postId);
   }
 
   /**
    * Tests construct() with empty institution.
    */
   public function testConstructNoInstitution() {
-    $person = new Person('Name', '', '111');
+    $person = new Person('Name', '', '1234');
 
     $this->assertEquals('Name', $person->name);
     $this->assertEquals('', $person->institution);
-    $this->assertEquals('111', $person->postId);
+    $this->assertEquals('1234', $person->postId);
   }
 
 }
